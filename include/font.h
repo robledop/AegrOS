0a961216e74fc4931c2859da2174e11916c87a9f
Robledo Pazotto <robledo@gmail.com>
2025-09-21 15:01:43 -0300

    Rename font arrays for consistency and clarity

--- include/font.h
+++ include/font.h
@@ -1,6 +1,5 @@
 #pragma once
 
-extern unsigned char font8x8_basic[128][8];
+extern unsigned char font8x8[128][8];
 extern unsigned char mouse_cursor[8][8];
-
-extern unsigned char font_array[];
+extern unsigned char font8x12[];
